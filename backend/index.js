8dd275d3fac746af8d735f9ece28e5a21edb24f0
Suraj Kumar Gupta <surajguptabnp14c@gmail.com>
2023-09-17 19:18:49 +0530

    backend initialized

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 
 const app = express();
-
 app.use(express.json());
 const PORT = 8080;
 
 app.listen(() => {
-  console.log("app is running on port", PORT);
+  console.log(`app is running on ${PORT}`);
 });
